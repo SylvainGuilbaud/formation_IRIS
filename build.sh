b36cb8dac75a3f0039b8b8c40d289d258d2b6860
SylvainGuilbaud <sylvain.guilbaud@intersystems.com>
2022-12-05 18:46:59 +0100

    volumes

---
+++ build.sh
@@ -0,0 +1,4 @@
+#env=production
+env=development 
+# docker-compose -f docker-compose-$env.yml -p formation-iris down --rmi all
+BUILDKIT_PROGRESS=plain docker-compose -f docker-compose-$env.yml -p formation-iris build --no-cache
