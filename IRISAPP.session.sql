d7e32c1b30bf03ad1303760a3efa488f0a4ed421
SylvainGuilbaud <sylvain.guilbaud@intersystems.com>
2024-01-25 10:10:35 +0100

    Sample

---
+++ IRISAPP.session.sql
@@ -0,0 +1,18 @@
+-- SELECT TOP 3 Name,
+-- JSON_ARRAY(%ID,%TABLENAME,UCASE(Name),Age,Home_State) "JSON data" 
+-- FROM Sample.Person
+
+-- SELECT TOP 3 Name,
+-- JSON_ARRAY(%ID,%TABLENAME,UCASE(Name),Age,Home_State ABSENT ON NULL
+-- ) "JSON data" 
+-- FROM Sample.Person
+
+-- SELECT JSON_ARRAY(Name,Age,Home_State) "JSON data"
+-- FROM Sample.Person
+
+-- SELECT TOP 3 JSON_OBJECT('title':'Person from','country':UCASE(Home_State),'table':%TABLENAME,'name':Name,'id':%ID,'age':Age) "JSON data" 
+-- FROM Sample.Person
+
+SELECT TOP 3 JSON_OBJECT('title':'Person from','country':UCASE(Home_State),'table':%TABLENAME,'name':Name,'id':%ID,'age':Age ABSENT ON NULL) "JSON data" 
+FROM Sample.Person
+
